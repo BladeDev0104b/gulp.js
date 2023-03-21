80d9b1a09119a3520a929ed99b2eceb87cb47903
BladeDev0104b <blade.dev0104b@gmail.com>
2023-03-21 21:39:06 +0200

    initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,17 @@
+//=================================================
+// JS scripts
+//
+// @package ProjectName
+// @author  Adveits <support@adveits.com>
+// @version 1.0.0
+//=================================================
+
+//=================================================
+// Default library
+//=================================================
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+//= ../../../node_modules/bs-custom-file-input/dist/bs-custom-file-input.js
+//= ../../../node_modules/outdated-browser-rework/dist/outdated-browser-rework.js
+
+//= ../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js
